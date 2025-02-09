0c1657fbdb9d4b5a2963583ce5f32d2334a803de
HenkeJohansson <henrik89johansson@gmail.com>
2025-02-09 20:59:06 +0100

    First draft for the layout.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FormComponent } from './components/form/form.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FormComponent],
+  imports: [RouterOutlet, HeaderComponent, FormComponent, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
