c7cc1accb0f5c61fed7b84aee29f918fde45f6ad
HenkeJohansson <henrik89johansson@gmail.com>
2025-02-12 14:54:08 +0100

    Added some styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, NavBarComponent],
+  imports: [RouterOutlet, HeaderComponent, NavBarComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'al-chef';
+
+  get isHomePage(): boolean {
+    return location.pathname === '/';
+  }
 }
